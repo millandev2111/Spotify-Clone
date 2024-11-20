1d35f23d3f4efabc9b050666d4e3f3f67e03e7c9
millandev2111 <millandev2111@gmail.com>
2024-11-20 18:55:39 -0500

    project-added

---
+++ src/components/CarPlayButton.jsx
@@ -0,0 +1,30 @@
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from "../store/playerStore"
+
+export function CarPlayButton ({id, size = 'small'}) {
+    const {currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
+    const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+    
+    const handleClick = () => {
+        if (isPlayingPlaylist) {
+            setIsPlaying(false)
+            return
+        }
+        fetch(`/api/get-info-playlist.json?id=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            const {songs, playlist} = data
+            setIsPlaying(true)
+            setCurrentMusic({songs, playlist, song: songs[0]})
+
+        })
+    }
+
+    const iconClassName = size === 'small' ? 'w-4 h4' : 'w-6 h-6'
+
+    return(
+    <button onClick={handleClick} className="card-play-button rounded-full bg-blue-900 p-4 hover:scale-105 transition hover:bg-blue-500   ">
+        {isPlayingPlaylist ? <Pause className={iconClassName}/> : <Play className={iconClassName}/>}
+    </button>
+    )
+}
